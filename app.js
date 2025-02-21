c7a32191c17389fe7167dace25356439a8e7d6fc
Md. Talha Hossain <talhahossain56@google.com>
2025-02-21 21:51:42 +0600

    complete layout & styling and add basic functionality.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-
-const homeStartingContent = "This is a simple blog website where you can write your daily thoughts and share it with the world. You can also read other people's blogs and get inspired. You can also delete your blog if you want to. You can also read the full blog by clicking on the Read More button.";
-const aboutContent = "This is a simple blog website where you can write your daily thoughts and share it with the world. You can also read other people's blogs and get inspired. You can also delete your blog if you want to. You can also read the full blog by clicking on the Read More button.";
-const contactContent = "This is a simple blog website where you can write your daily thoughts and share it with the world. You can also read other people's blogs and get inspired. You can also delete your blog if you want to. You can also read the full blog by clicking on the Read More button.";
+const homeStartingContent = `Horem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`;
+const aboutContent = `Aorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`;
+const contactContent = `Corem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`;
 
 const app = express();
 
@@ -13,10 +21,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res) {
-    res.render("home");
+    res.render("home", {startingContent: homeStartingContent, posts: posts});
 });
 
+app.get("/about", function(req, res) {
+    res.render("about", {aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact", {contactContent});
+});
+
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    };
+
+    posts.push(post);
+    res.redirect("/");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
